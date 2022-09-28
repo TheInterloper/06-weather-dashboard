ac1daa32c5ad0beef223b30b5518c96352540009
Tyler Olson <t19.olson@gmail.com>
2022-09-27 21:09:07 -0500

    API working but no display. Search working. wireframing layout.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 
-var cityName = 
+var cityName = document.getElementById("searchfield")
+var searchButton = document.getElementById("searchbtn")
+var currentWeather = document.getElementById("currentweather")
+var forecast = document.getElementById("")
 
-function getApi(){
-  var requestApi = 'https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=56e4f4879148dccce2da9757a8e2f7f6'
+function getApi(event){
+  event.preventDefault()
+  var city = cityName.value
+  console.log(city)
+  var requestApi = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=56e4f4879148dccce2da9757a8e2f7f6&units=imperial`
 
   //initiate API request
   fetch(requestApi)
@@ -11,29 +17,76 @@ function getApi(){
     })
 
     .then(function (data) {
-
+      currentWeather(data)
+      //weather 5 day here
+      console.log(data)
     })
+    
 }
+//dispaly todays weather in the large card
+// function currentWeather(data) {
+//   var todayWeather = document.createElement('div')
+//   var cityName = document.createElement('div')
+//   var date = document.createElement('div')
+//   var icon = document.createElement('img')
+//   var temp = document.createElement('div')
+//   var humdity = document.createElement('div')
+//   var windSpeed = document.createElement('div')
+
+// };
+
+
+
+// loop over the data in incrents of 7 to offset the 3hr breakdown get 1 day increments
+//display each day in a small card below the large card
+// function weather5Day(data) {
+//   for (var i = 7; i < data.list.length; i +=7)
+//     var small = document.createElement('div')
+//     var cityName = document.createElement('div')
+//     var date = document.createElement('div')
+//     var icon = document.createElement('img')
+//     var temp = document.createElement('div')
+//     var humdity = document.createElement('div')
+//     var windSpeed = document.createElement('div')
+
+// };
+
+
+
+
+
+
+
+
+
+
+
+searchButton.addEventListener("click", getApi)
+
+
+
+
+
 
 
 
 //Code below provided by instructor as a workaround to how the weather API works.
-const weatherDays = []  
-let currDay = null
+// const weatherDays = []  
+// let currDay = null
 
-sampleData.list.forEach( function(tsObj){
+// sampleData.list.forEach( function(tsObj){
 
-  // Makes a moment date object for each record
-  const dateObj = moment.unix(tsObj.dt)
+//   // Makes a moment date object for each record
+//   const dateObj = moment.unix(tsObj.dt)
 
-  // Generate the day # for the day in the date object
-  const dateNum = dateObj.format("DDD")
+//   // Generate the day # for the day in the date object
+//   const dateNum = dateObj.format("DDD")
 
-  // If the current date in tsObj hasn't had a record put into weatherDays, do that now 
-  // Then skip over all other records for this day
-  if( dateNum !== currDay && weatherDays.length < 5 ){
-    weatherDays.push( tsObj )
-    currDay = dateNum
-  }
+//   // If the current date in tsObj hasn't had a record put into weatherDays, do that now 
+//   // Then skip over all other records for this day
+//   if( dateNum !== currDay && weatherDays.length < 5 ){
+//     weatherDays.push( tsObj )
+//     currDay = dateNum
+//   }
 
-})
+// })
